9f2c8bff582b44c6ca042260613edb8c9c1e7c18
CHANDRAKANT TIWARI <chandrakant91550@gmail.com>
2023-03-04 19:15:00 +0530

    first commit

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+            process.env.REACT_APP_YOUTUBE_API_KEY || "473c4441b8msha2d38deee17025cp1435b4jsnfadcc140d231",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
